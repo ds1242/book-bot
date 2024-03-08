d0fb3324cca67505ff980224c19a454a20ec93ac
David Shaw <dshaw11@utk.edu>
2024-03-08 15:54:15 -0500

    created a function to convert the words into a list and then provide how many letters exist in a dictionary

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
         text = get_book_text(book_path)
         count = get_num_words(text)
         print(f'{count} is the number of words in the document')
+        letter_count = count_letters(text)
+        print(letter_count)
 
 
 def get_book_text(path):
@@ -13,5 +15,16 @@ def get_book_text(path):
 def get_num_words(text):
      words = text.split()
      return len(words)
+
+def count_letters(text):
+    count_dict = {}
+    text = text.lower()
+    chars = list(text)
+    for char in chars:
+        if char in count_dict:
+            count_dict[char] += 1
+        else:
+            count_dict[char] = 1
+    return count_dict
 
 main()
