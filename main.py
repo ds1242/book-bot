e2403287ced5440b3a0a8944e3f845bfe269309a
David Shaw <dshaw11@utk.edu>
2024-03-08 15:25:26 -0500

    broke the operations down into smaller functions

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+        book_path = 'books/frankenstein.txt'
+        text = get_book_text(book_path)
+        count = get_num_words(text)
+        print(f'{count} is the number of words in the document')
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def get_num_words(text):
+     words = text.split()
+     return len(words)
 
 main()
